23457edb93bc833563a4ad98fc7ee37604368acc
marmooo <marmooo@users.noreply.github.com>
2021-10-17 13:49:45 +0900

    bump bootstrap from 5.1.2 to 5.1.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-10-15 20:33';
+var CACHE_NAME = '2021-10-17 13:42';
 var urlsToCache = [
   "/type-numbers/",
   "/type-numbers/kohacu.webp",
@@ -8,7 +8,7 @@ var urlsToCache = [
   "/type-numbers/mp3/cat.mp3",
   "/type-numbers/mp3/correct3.mp3",
   "/type-numbers/favicon/original.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
